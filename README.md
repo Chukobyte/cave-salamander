3680d1a221bae2373838cc9cb560ed9e3803c63f
chukobyte <chukobyte@gmail.com>
2021-07-09 15:42:15 -0400

    Initial commit for uniday jam 2021.

---
+++ README.md
@@ -0,0 +1,3 @@
+## Uniday Jam 2021
+
+A work in progress game for uniday jam.
---
+++ src/init.py
@@ -0,0 +1,5 @@
+from seika.node import Node2D
+
+class Init(Node2D):
+   def _start(self) -> None:
+       print("Uniday Jam!")
